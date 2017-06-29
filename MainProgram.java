baa44da90f3c7e3bcec5186153e51f6f952cedf0
gabemadison <gabe.madison96@gmail.com>
2017-06-29 19:49:01 -0400

    Implement options for math program with switch.

---
+++ MainProgram.java
@@ -0,0 +1,12 @@
+public class MainProgram {
+  public static void main(String[] args) {
+    System.out.println("Welcome to the math programming emporium!\n");
+    System.out.println("To begin, choose an option from below!");
+
+    int option = 0;
+    System.out.println("\n(1) Addition\n(2) Subtraction\n");
+    switch(option) {
+      case 1:
+    }
+  }
+}
